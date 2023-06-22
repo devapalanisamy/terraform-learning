2009ab41b5482a70973c1dc640b1e5b3771d4de6
Deva <dev.palanisamy@gmail.com>
2023-06-22 12:36:04 +0100

    updated 2

--- 01_hello_world/main.tf
+++ 01_hello_world/main.tf
@@ -1,13 +1,11 @@
 provider "aws" {
   region = "eu-west-2"
-    access_key = "***"
-  secret_key = "***"
 }
 
 resource "aws_instance" "example" {
   ami           = "ami-07650ecb0de9bd731"
   instance_type = "t3.micro"
   tags = {
-    Name = "oak-tree"
+    Name = "oak-tree2"
   }
 }
