6db846bf7a27bdcf53575066e9eb42eae0e4b787
Deva <dev.palanisamy@gmail.com>
2023-06-27 12:51:53 +0100

    Update main.tf

--- 01_hello_world/main.tf
+++ 01_hello_world/main.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "example" {
     #!/bin/bash
     set -ex
     sudo yum update -y
-    sudo amazon-linux-extras install docker -y
+    sudo yum install docker -y
     sudo service docker start
     sudo usermod -a -G docker ec2-user
     sudo curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
