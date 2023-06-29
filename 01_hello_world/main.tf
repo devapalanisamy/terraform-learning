17e643172084a536ee44a54b8e12022fbfdf8b0d
Deva <dev.palanisamy@gmail.com>
2023-06-29 12:44:44 +0100

    Update main.tf

--- 01_hello_world/main.tf
+++ 01_hello_world/main.tf
@@ -18,6 +18,7 @@ resource "aws_instance" "example" {
     sudo usermod -a -G docker ec2-user
     sudo curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
+    sudo docker run tkonline19/webpage 
   EOF
 
 }
